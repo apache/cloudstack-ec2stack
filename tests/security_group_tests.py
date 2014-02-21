18cf9eb7c1090d28af674b29ba19ba1224608854
Ian Duffy <ian@ianduffy.ie>
2014-02-21 03:37:23 +0000

    Add files for several tests

---
+++ tests/security_group_tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import mock
+
+from ec2stack.helpers import read_file, generate_signature
+from . import Ec2StackAppTestCase
+
+
+#class SecurityGroupTestCase(Ec2StackAppTestCase):
