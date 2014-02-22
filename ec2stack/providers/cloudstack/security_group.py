e28cee2460b93cebbe38cb4092119084f1b9daef
Ian Duffy <ian@ianduffy.ie>
2014-02-22 12:23:10 +0000

    [WIP] add async job support + very broken authorize ingress

--- ec2stack/providers/cloudstack/security_group.py
+++ ec2stack/providers/cloudstack/security_group.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from flask import request
+from flask import request, abort
 
-from ec2stack import helpers
-from ec2stack.helpers import authentication_required
+from ec2stack.helpers import authentication_required, get, \
+    contains_parameter, missing_paramater, require_one_paramater, require_parameters
 from ec2stack.core import Ec2stackError
 from ec2stack.providers.cloudstack import requester
 
 
 @authentication_required
 def create_security_group():
-    helpers.require_parameters(['GroupName', 'GroupDescription'])
+    require_parameters(['GroupName', 'GroupDescription'])
     response = _create_security_group_request()
     return _create_security_group_response(response)
 
@@ -19,8 +19,8 @@ def create_security_group():
 def _create_security_group_request():
     args = {}
     args['command'] = 'createSecurityGroup'
-    args['name'] = helpers.get('GroupName', request.form)
-    args['description'] = helpers.get('GroupDescription', request.form)
+    args['name'] = get('GroupName', request.form)
+    args['description'] = get('GroupDescription', request.form)
 
     response = requester.make_request(args)
 
@@ -31,7 +31,7 @@ def _create_security_group_request():
 
 def _create_security_group_response(response):
     if 'errortext' in response:
-        groupname = helpers.get('GroupName', request.form)
+        groupname = get('GroupName', request.form)
         raise Ec2stackError(
             '400',
             'InvalidGroupName.Duplicate',
@@ -56,14 +56,14 @@ def delete_security_group():
 def _delete_security_group_request():
     args = {}
 
-    if helpers.contains_parameter('GroupName'):
-        args['name'] = helpers.get('GroupName', request.form)
+    if contains_parameter('GroupName'):
+        args['name'] = get('GroupName', request.form)
 
-    elif helpers.contains_parameter('GroupId'):
-        args['id'] = helpers.get('GroupId', request.form)
+    elif contains_parameter('GroupId'):
+        args['id'] = get('GroupId', request.form)
 
     else:
-        helpers.missing_paramater('GroupName')
+        missing_paramater('GroupName')
 
     args['command'] = 'deleteSecurityGroup'
 
@@ -78,3 +78,34 @@ def _delete_security_group_response(response):
         'response_type': 'DeleteSecurityGroupResponse',
         'return': 'true'
     }
+
+
+@authentication_required
+def authenticate_security_group_ingress():
+    require_one_paramater(['GroupName', 'GroupId'])
+    _authenticate_security_group_request()
+    return abort(400)
+    #return _authenticate_security_group_response(response)
+
+
+def _authenticate_security_group_request(args=None):
+    if not args:
+        args = {}
+
+    args['command'] = 'authorizeSecurityGroupIngress'
+
+    if contains_parameter('GroupName'):
+        args['securityGroupName'] = get('GroupName', request.form)
+    elif contains_parameter('GroupId'):
+        args['securityGroupId'] = get('GroupId', request.form)
+
+    args['protocol'] = get('IpPermissions.1.IpProtocol', request.form)
+    args['startPort'] = get('FromPort', request.form)
+    args['endPort'] = get('ToPort', request.form)
+    args['cidrlist'] = get('CidrIp', request.form)
+
+    response = requester.make_request_async(args)
+
+    response = response['securitygroup']
+
+    return response
