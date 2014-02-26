c719731c1c8acaa755d107918077e810182111ae
Ian Duffy <ian@ianduffy.ie>
2014-02-26 01:05:06 +0000

    Generificy images

--- tests/images_tests.py
+++ tests/images_tests.py
@@ -49,31 +49,11 @@ class ImagesTestCase(Ec2StackAppTestCase):
         self.assertOk(response)
         assert 'DescribeImagesResponse' in response.data
 
-    #def test_invalid_image_id(self):
-    #    data = self.get_example_data()
-    #    data['Action'] = 'DescribeImages'
-    #    data['ImageId.1'] = 'InvalidId'
-    #    data['Signature'] = generate_signature(data, 'POST', 'localhost')
-
-    #    get = mock.Mock()
-    #    text = get.return_value.text = read_file(
-    #        'tests/data/invalid_image_id.json'
-    #    )
-    #    status_code = get.return_value.status_code = 431
-
-    #    with mock.patch('requests.get', get):
-    #        response = self.post(
-    #            '/',
-    #            data=data
-    #        )
-
-    #    self.assertBadRequest(response)
-    #    assert 'InvalidItemId.Malformed' in response.data
 
     def test_describe_image_attribute(self):
         data = self.get_example_data()
         data['Action'] = 'DescribeImageAttribute'
-        data['ImageId.1'] = 'a32d70ee-95e4-11e3-b2e4-d19c9d3e5e1d'
+        data['ImageId'] = 'a32d70ee-95e4-11e3-b2e4-d19c9d3e5e1d'
         data['Attribute'] = 'isready'
         data['Signature'] = generate_signature(data, 'POST', 'localhost')
 
@@ -88,6 +68,8 @@ class ImagesTestCase(Ec2StackAppTestCase):
                 '/',
                 data=data
             )
+
+        print response.data
 
         self.assertOk(response)
         assert 'DescribeImageAttributeResponse' in response.data
