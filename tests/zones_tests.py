203d18037578f9516e21941600df72beffbbb66a
Ian Duffy <ian@ianduffy.ie>
2014-03-04 14:22:17 +0000

    Format code

--- tests/zones_tests.py
+++ tests/zones_tests.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import mock
-
 import json
+
+import mock
 
 from ec2stack.helpers import read_file, generate_signature
 from . import Ec2StackAppTestCase
