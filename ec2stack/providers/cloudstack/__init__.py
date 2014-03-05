b14bd182ffec9cca6edde881d46fde9c3aaf552f
BroganD1993 <darrenbrogan@hotmail.com>
2014-03-05 19:40:15 +0000

    Return empty set, instead of error if description of items is empty

--- ec2stack/providers/cloudstack/__init__.py
+++ ec2stack/providers/cloudstack/__init__.py
@@ -9,7 +9,7 @@ def describe_item(args, keyname, not_found, prefix):
     if helpers.contains_parameter_with_keyword(prefix):
         response = _describe_specific_item(args, keyname, not_found, prefix)
     else:
-        response = describe_items_request(args, not_found)
+        response = _describe_items_request(args, {})
 
     return response
 
@@ -34,24 +34,26 @@ def _describe_specific_item(args, keyname, not_found, prefix):
 
 
 def describe_item_request(args, keyname, not_found):
-    request = describe_items_request(args, not_found)
+    request = _describe_items_request(args, not_found)
     request = request[keyname]
 
     for item in request:
-        if 'id' in args and args['id'].lower() == item['id'].lower():
+        if 'id' in args and args['id'] == item['id']:
             return item
-        elif 'name' in args and args['name'].lower() == item['name'].lower():
+        elif 'name' in args and args['name'] == item['name']:
             return item
 
     return not_found()
 
 
-def describe_items_request(args, not_found):
+def _describe_items_request(args, not_found):
     args['listAll'] = 'true'
     response = requester.make_request(args)
     response = response[response.keys()[0]]
 
     if 'count' in response:
         return response
-    else:
+    elif callable(not_found):
         return not_found()
+    else:
+        return not_found
