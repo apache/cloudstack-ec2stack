882dc5fd52f50ba65c8ef8db3b90547141f957b1
Ian Duffy <ian@ianduffy.ie>
2015-04-13 14:49:47 +0100

    fix pep8 issues

--- ec2stack/__init__.py
+++ ec2stack/__init__.py
@@ -125,9 +125,7 @@ def _config_from_config_profile(config_file, profile, app):
     config.read(config_file)
 
     if not config.has_section(profile):
-        sys.exit('No profile matching ' + profile
-                 + ' found in configuration, please run ec2stack-configure -p '
-                 + profile)
+        sys.exit('No profile matching ' + profile + ' found in configuration, please run ec2stack-configure -p ' + profile)
 
     for attribute in config.options(profile):
         app.config[attribute.upper()] = config.get(profile, attribute)
